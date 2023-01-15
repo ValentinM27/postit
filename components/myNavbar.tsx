c2b4e56b828cb8eb4234d9faeaec55827c7be774
Valentin Marguerie <74249732+ValentinM27@users.noreply.github.com>
2023-01-15 21:01:46 +0100

    [front] - responsive navbar

--- components/myNavbar.tsx
+++ components/myNavbar.tsx
@@ -1,7 +1,11 @@
 import usersServices from "../services/users.services";
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
 const myNavbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [largeur, setLargeur] = useState(window.innerWidth);
+
   const router = useRouter();
 
   const handleLogout = () => {
@@ -9,30 +13,68 @@ const myNavbar = () => {
     router.push("/users/signup");
   };
 
+  const toggleNavSmallScreen = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setLargeur(window.innerWidth);
+
+      if (window.innerWidth > 768) {
+        setToggleMenu(false);
+      }
+    };
+
+    window.addEventListener("resize", changeWidth);
+
+    return () => {
+      window.removeEventListener("resize", changeWidth);
+    };
+  }, []);
+
   return (
-    <nav className="header">
-      <div className="left-header">
-        <a href="/">The Archiver</a>
+    <nav className="header navbar navbar-dark bg-dark">
+      <div className="left-container-header d-flex justify-content-between">
+        <div className="left-header">
+          <a href="/">The Archiver</a>
+        </div>
+        {largeur <= 768 && (
+          <div>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarToggleExternalContent"
+              aria-controls="navbarToggleExternalContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+              onClick={toggleNavSmallScreen}
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+          </div>
+        )}
       </div>
-      <div className="right-header">
-        <div>
-          <a href="/">Archives</a>
+      {(toggleMenu || largeur > 768) && (
+        <div className="right-header">
+          <div>
+            <a href="/">Archives</a>
+          </div>
+          <div>
+            <a href="/users/profil">Profil</a>
+          </div>
+          <div>
+            <button
+              onClick={() => handleLogout()}
+              type="button"
+              className="btn btn-light"
+            >
+              Log out
+            </button>
+          </div>
         </div>
-        |
-        <div>
-          <a href="/users/profil">Profil</a>
-        </div>
-        |
-        <div>
-          <button
-            onClick={() => handleLogout()}
-            type="button"
-            className="btn btn-light"
-          >
-            Log out
-          </button>
-        </div>
-      </div>
+      )}
     </nav>
   );
 };
