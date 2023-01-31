cec8e83364d2414003b17f6483656fc72734bba9
Valentin Marguerie <74249732+ValentinM27@users.noreply.github.com>
2023-01-31 23:37:29 +0100

    [front] - Add loader => Read book && fetch bookRefs

--- components/book.tsx
+++ components/book.tsx
@@ -18,8 +18,14 @@ const book = (props: any) => {
   };
 
   const handleFetch = async (bookId: string) => {
-    const book: any = await booksServices.getBook(bookId);
-    props?.readBook(book, props?.book);
+    try {
+      setIsWaitingApi(true);
+      const book: any = await booksServices.getBook(bookId);
+      props?.readBook(book, props?.book);
+    } catch (error) {
+      setIsWaitingApi(false);
+      console.log(error);
+    }
   };
 
   const handleFetchDelete = async (bookId: string) => {
@@ -42,33 +48,27 @@ const book = (props: any) => {
         backgroundPosition: "center",
       }}
     >
-      {isWaitingApi ? (
-        <div className="delete-btn-container m-2">
-          <Loader />
-        </div>
-      ) : (
-        <div className="delete-btn-container">
-          <button
-            onClick={(event) => handleDelete(props?.book?._id)}
-            className="btn btn-danger m-2"
+      <div className={isWaitingApi ? "hide" : "delete-btn-container"}>
+        <button
+          onClick={(event) => handleDelete(props?.book?._id)}
+          className="btn btn-danger m-2"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-trash"
+            viewBox="0 0 16 16"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-trash"
-              viewBox="0 0 16 16"
-            >
-              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
-              <path
-                fillRule="evenodd"
-                d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
-              />
-            </svg>
-          </button>
-        </div>
-      )}
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+            <path
+              fillRule="evenodd"
+              d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+            />
+          </svg>
+        </button>
+      </div>
       {(!props?.book?.cover || props?.book?.cover?.length === 0) && (
         <div className="title-container">
           <h2>{props?.book?.title}</h2>
@@ -76,7 +76,19 @@ const book = (props: any) => {
         </div>
       )}
 
-      <div className="btn-container col-12 d-flex justify-content-center">
+      {isWaitingApi && (
+        <div className="center">
+          <Loader />
+        </div>
+      )}
+
+      <div
+        className={
+          isWaitingApi
+            ? "hide"
+            : "btn-container col-12 d-flex justify-content-center"
+        }
+      >
         <button
           onClick={(event) => handleRead(props?.book?._id)}
           className="btn btn-dark btn-lg m-2"
